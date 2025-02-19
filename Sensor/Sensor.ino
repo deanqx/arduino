3fe0609e310fdab09c2dd6c033b14afbf163aa0d
deanqx <110404616+deanqx@users.noreply.github.com>
2025-02-19 09:04:49 +0100

    Added switches

--- Sensor/Sensor.ino
+++ Sensor/Sensor.ino
@@ -1,11 +1,34 @@
+/*
+    TI_60_Taster_Check
+    Dies ist eine Ampelschaltung. Wenn es dunkel wird
+    wechselt es in einen Blink-Modus. Mit den Tastern
+    ...
+
+    Pins mit LA endung steht für Low Active,
+    sprich wenn es Aktiviert ist, ist das Signal LOW
+    bzw. wenn es Aktiviert werden soll, muss das Signal LOW
+
+    Name                AVR-Pin
+    PIN_DRIVE_GREEN     PD2
+    PIN_DRIVE_YELLOW    PD3
+    PIN_DRIVE_RED       PD4
+    PIN_SWITCH          PD5
+    PIN_SWITCH_LA       PD6
+    PIN_BRIGHT          PD7
+    PIN_WALK_GREEN_LA   PB2
+    PIN_WALK_RED_LA     PB3
+*/
+
+// Arduino Pins:
 #define PIN_DRIVE_GREEN 2
 #define PIN_DRIVE_YELLOW 3
 #define PIN_DRIVE_RED 4
+#define PIN_SWITCH 5
+#define PIN_SWITCH_LA 6
 #define PIN_BRIGHT 7
 #define PIN_WALK_GREEN_LA 10
 #define PIN_WALK_RED_LA 11
 
-// Pins für Füßgänger sind Low Active (LA)
 
 void setup()
 {
@@ -13,15 +36,17 @@ void setup()
     Serial.begin(9600);
 
     // Senden über Seriell
-    Serial.println("Hallo");
+    Serial.println("Ampel mit Tag und Nachtmodus");
 
-    // Setze Pins auf Ausgang oder Eingang 
-    pinMode(PIN_WALK_GREEN_LA, OUTPUT);
-    pinMode(PIN_WALK_RED_LA, OUTPUT);
+    // Setze Pins auf Ausgang oder Eingang
     pinMode(PIN_DRIVE_GREEN, OUTPUT);
     pinMode(PIN_DRIVE_YELLOW, OUTPUT);
     pinMode(PIN_DRIVE_RED, OUTPUT);
+    pinMode(PIN_SWITCH, INPUT);
+    pinMode(PIN_SWITCH_LA, INPUT);
     pinMode(PIN_BRIGHT, INPUT);
+    pinMode(PIN_WALK_GREEN_LA, OUTPUT);
+    pinMode(PIN_WALK_RED_LA, OUTPUT);
 
     // Fußgänger LEDs auf HIGH ausgeschalten
     digitalWrite(PIN_WALK_GREEN_LA, HIGH);
@@ -36,6 +61,28 @@ int received_byte = 0;
 
 void loop()
 {
+    if (digitalRead(PIN_SWITCH))
+    {
+        Serial.println("Taste S1 in Positiv-Logik wurde gedrückt");
+        digitalWrite(PIN_DRIVE_RED, HIGH);
+    }
+    else
+    {
+        digitalWrite(PIN_DRIVE_RED, LOW);
+    }
+
+    if (!digitalRead(PIN_SWITCH_LA))
+    {
+        Serial.println("Taste S2 in Negativ-Logik wurde gedrückt");
+        digitalWrite(PIN_DRIVE_GREEN, HIGH);
+    }
+    else
+    {
+        digitalWrite(PIN_DRIVE_GREEN, LOW);
+    }
+
+    return;
+
     // Wurde etwas geschickt?
     if (Serial.available())
     {
