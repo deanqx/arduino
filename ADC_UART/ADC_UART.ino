0a8e71f016be65548194cfa65cec4754905bdf76
deanqx <110404616+deanqx@users.noreply.github.com>
2025-03-05 19:40:57 +0100

    Working on ADC

--- ADC_UART/ADC_UART.ino
+++ ADC_UART/ADC_UART.ino
@@ -1,55 +1,81 @@
-/*
-  Analog Input
+// Pin-Belegung (A: analog)
+#define A_NTC A0
+#define A_LDR A1
+#define A_POTI A2
 
-  Demonstrates analog input by reading an analog sensor on analog pin 0 and
-  turning on and off a light emitting diode(LED) connected to digital pin 13.
-  The amount of time the LED will be on and off depends on the value obtained
-  by analogRead().
+uint16_t iteration = 0;
 
-  The circuit:
-  - potentiometer
-    center pin of the potentiometer to the analog input 0
-    one side pin (either one) to ground
-    the other side pin to +5V
-  - LED
-    anode (long leg) attached to digital output 13 through 220 ohm resistor
-    cathode (short leg) attached to ground
-
-  - Note: because most Arduinos have a built-in LED attached to pin 13 on the
-    board, the LED is optional.
-
-  created by David Cuartielles
-  modified 30 Aug 2011
-  By Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogInput
-*/
-
-int sensorPin = A2;   // select the input pin for the potentiometer
-int ledPin = 13;       // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
-
-void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
-
+void setup()
+{
   Serial.begin(9600);
 }
 
-void loop() {
-  // read the value from the sensor:
-  sensorValue = analogRead(sensorPin);
+// Sensor Spannung in mV analog lesen
+uint16_t read_voltage(const uint8_t analog_pin)
+{
+  // (Analog Wert * Gesamt Spannung) / Maximaler Analog Wert (10 bit)
 
-  Serial.println(sensorValue);
+  return analogRead(analog_pin) * 5000 / 1023;
+}
 
-  // turn the ledPin on
-  digitalWrite(ledPin, HIGH);
-  // stop the program for <sensorValue> milliseconds:
-  delay(sensorValue);
-  // turn the ledPin off:
-  digitalWrite(ledPin, LOW);
-  // stop the program for <sensorValue> milliseconds:
-  delay(sensorValue);
+// Sensor Widerstand in Ohm lesen
+uint16_t read_resistance(const uint8_t analog_pin, const uint16_t sensor_mv)
+{
+  /*
+   Vorwiderstand * Sensor Spannung / (Gesamtspannung - Sensor Spannung)
+
+   Hergeleitet aus:
+     Sensor Spannung = Gesamt Spannung * Sensor Widerstand
+                       / (Vorwiderstand + Sensor Widerstand)
+  */
+
+  return 10000 * sensor_mv / (5000 - sensor_mv);
+}
+
+void loop()
+{
+  const uint16_t ntc_mv = read_voltage(A_NTC);
+  const uint16_t ldr_mv = read_voltage(A_LDR);
+  const uint16_t poti_mv = read_voltage(A_POTI);
+
+  Serial.print("Messung: ");
+  Serial.print(iteration);
+  Serial.print("\tNTC: ");
+  Serial.print(ntc_mv);
+  Serial.print("mv\tLDR: ");
+  Serial.print(ldr_mv);
+  Serial.print("mv\tPoti: ");
+  Serial.print(poti_mv);
+  Serial.println("mv");
+
+  const uint16_t ntc_ohm = read_resistance(A_NTC, ntc_mv);
+  const uint16_t ldr_ohm = read_resistance(A_LDR, ldr_mv);
+  const uint16_t poti_ohm = read_resistance(A_POTI, poti_mv);
+
+  Serial.print("Messung: ");
+  Serial.print(iteration);
+  Serial.print("\tNTC: ");
+  Serial.print(ntc_ohm);
+  Serial.print(" Ohm\tLDR: ");
+  Serial.print(ldr_ohm);
+  Serial.print(" Ohm\tPoti: ");
+  Serial.print(poti_ohm);
+  Serial.println(" Ohm");
+
+  // TODO: Temperature, Lux, Distance
+  const uint16_t ntc_ohm = read_resistance(A_NTC, ntc_mv);
+  const uint16_t ldr_ohm = read_resistance(A_LDR, ldr_mv);
+  const uint16_t poti_ohm = read_resistance(A_POTI, poti_mv);
+
+  Serial.print("Messung: ");
+  Serial.print(iteration);
+  Serial.print("\tNTC: ");
+  Serial.print(ntc_ohm);
+  Serial.print(" Ohm\tLDR: ");
+  Serial.print(ldr_ohm);
+  Serial.print(" Ohm\tPoti: ");
+  Serial.print(poti_ohm);
+  Serial.println(" Ohm");
+  delay(500);
+  iteration++;
 }
