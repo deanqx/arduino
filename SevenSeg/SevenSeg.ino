29ddcda3286ae7fe106495b04d6d7b9b19ba4840
deanqx <110404616+deanqx@users.noreply.github.com>
2025-03-21 10:46:13 +0100

    Added 4 segments, searching bug

--- SevenSeg/SevenSeg.ino
+++ SevenSeg/SevenSeg.ino
@@ -1,70 +1,80 @@
 /*  TI_63_7Seg_ComK
     7 Segment Display Test mit 5161AS
     LEDs sind Positivaktiv
-    Vorwiderstand für LEDs jeweils 220 Ohm
+    Vorwiderstand für LEDs jeweils 330 Ohm
 
     Pin Belegung
-    Bauteilkomponente   BauteilPin  ArduinoPin  MC_Pin
-    A                   7           2           PD2
-    B                   6           3           PD3
-    C                   4           4           PD4
-    D                   2           5           PD5
-    E                   1           6           PD6
-    F                   9           7           PD7
-    G                   10          8           PB0
-    DP                  5           9           PB1
-    GND                 3 oder 8    GND         GND
+    LED   Einzelsegment Pin  Vierersegment Pin  ArduinoPin  MC_Pin
+    A     7                  11                 8           PB0
+    B     6                  7                  9           PB1
+    C     4                  4                  10          PB2
+    D     2                  2                  11          PB3
+    E     1                  1                  4           PD4
+    F     9                  10                 5           PD5
+    G     10                 5                  6           PD6
+    DP    5                  3                  7           PD7
+    GND0                     6                  A0          PC0
+    GND1                     8                  A1          PC1
+    GND2                     9                  A2          PC2
+    GND3                     12                 A3          PC3
+    GND4  8                                     A4          PC4
 */
 
 // Arduino Pins
-#define A 2
-#define B 3
-#define C 4
-#define D 5
-#define E 6
-#define F 7
-#define G 8
-#define DP 9
+#define A 4
+#define B 5
+#define C 6
+#define D 7
+#define E 8
+#define F 9
+#define G 10
+#define DP 11
 
-// MC Pins
-#define A_D PB0
+#define TURN_OFF_DISPLAY 10
 
-uint8_t current_number = -1;
+uint16_t current_number = 0;
 
-// An und Auszustände für 7 Segment LEDs: A - DP 
-typedef unsigned char DiceMap;
-
-// Zuweisungsliste: Zahl (0 - 9) zu LED Zustände; [10] => aus
-const DiceMap segment_assignments[] = {
-  0b11111100,
-  0b11111100,
-  0b01100000,
-  0b11110010,
+/* Zuweisungsliste: Zahl (0 - 9) zu LED Zustände; [10] => aus
+ 0b[PD7 - PD4][PB3 - PB0]
+ 0b[DP,G,F,E,D,C,B,A]
+*/
+const uint8_t segment_assignments[] = {
+  0b00111111,
+  0b00000110,
+  0b01011011,
+  0b01001111,
   0b01100110,
-  0b10110110,
-  0b10111110,
-  0b11100000,
-  0b11111110,
-  0b11110110
-  0b00000000
+  0b01101101,
+  0b01111101,
+  0b00000111,
+  0b01111111,
+  0b01101111,
   };
 
 /* Zeigt Zahl auf 7 Segment Display
-@returns 0=Erfolgreich; 1=Fehler
+@returns 0 Erfolgreich; >1 Fehler
 */ 
-int display_one_number(uint8_t number)
+uint8_t display_one_number(uint8_t number, uint8_t decimal_place)
 {
   if (number > 9)
   {
     return 1;
   }
 
-  // LED Zustand Code
-  DiceMap segment_assigment = segment_assigments[number];
+  if (decimal_place > 4)
+  {
+    return 2;
+  }
 
-  //  
-  DiceMap pins_0 = segment_assigment & 0b11110000;
-  DiceMap pins_1 = segment_assigment & 0b00001111;
+  PORTC = ~(1 << decimal_place);
+
+  // LED Zustand Code
+  uint8_t segment_assignment = segment_assignments[number];
+
+  // Using PB0 to PB3
+  PORTB = PORTB & 0xF0 | segment_assignment & 0x0F;
+  // Using PD4 to PD7
+  PORTD = PORTD & 0x10001111 | segment_assignment & 0b01110000;
 
   return 0;
 }
@@ -82,75 +92,35 @@ void setup()
   pinMode(F, OUTPUT);
   pinMode(G, OUTPUT);
   pinMode(DP, OUTPUT);
+  pinMode(A0, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A2, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A4, OUTPUT);
+  digitalWrite(A0, 1);
+  digitalWrite(A1, 1);
+  digitalWrite(A2, 1);
+  digitalWrite(A3, 1);
+  digitalWrite(A4, 1);
 
-  display_number(11);
-}
-
-void test_lauflicht()
-{
-  Serial.println("Lauflicht");
-  digitalWrite(A, 0);
-  digitalWrite(B, 0);
-  digitalWrite(C, 0);
-  digitalWrite(D, 0);
-  digitalWrite(E, 0);
-  digitalWrite(F, 0);
-  digitalWrite(G, 0);
-  digitalWrite(DP, 0);
-  delay(500);
-  digitalWrite(A, 1);
-  delay(500);
-  digitalWrite(A, 0);
-  digitalWrite(B, 1);
-  delay(500);
-  digitalWrite(B, 0);
-  digitalWrite(C, 1);
-  delay(500);
-  digitalWrite(C, 0);
-  digitalWrite(D, 1);
-  delay(500);
-  digitalWrite(D, 0);
-  digitalWrite(E, 1);
-  delay(500);
-  digitalWrite(E, 0);
-  digitalWrite(F, 1);
-  delay(500);
-  digitalWrite(F, 0);
-  digitalWrite(G, 1);
-  delay(500);
-  digitalWrite(G, 0);
-  digitalWrite(DP, 1);
-  delay(500);
-  digitalWrite(DP, 0);
-
-  Serial.println("Blinken");
-  for (uint8_t i = 0; i < 3 * 2; i++)
-  {
-    delay(500);
-    digitalWrite(A, !digitalRead(A));
-    digitalWrite(B, !digitalRead(B));
-    digitalWrite(C, !digitalRead(C));
-    digitalWrite(D, !digitalRead(D));
-    digitalWrite(E, !digitalRead(E));
-    digitalWrite(F, !digitalRead(F));
-    digitalWrite(G, !digitalRead(G));
-    digitalWrite(DP, !digitalRead(DP));
-  }
+  // display_one_number(TURN_OFF_DISPLAY, 0);
 }
 
 void loop()
 {
-  test_lauflicht();
-  return;
-
-  current_number++;
-
-  if (current_number > 9)
+  if (current_number > 9999)
   {
     current_number = 0;
   }
 
-  display_number(current_number);
-  delay(500);
+  uint16_t digit_10 = current_number % 10;
+  uint16_t digit_100 = (current_number / 10) % 10;
+
+  display_one_number(0, 0);
+  delay(1);
+  display_one_number(1, 1);
+  delay(200);
+
+  current_number++;
 }
 
