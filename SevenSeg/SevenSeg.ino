16ac414895e677050b83845ce6259affc843d16c
deanqx <110404616+deanqx@users.noreply.github.com>
2025-03-31 17:45:45 +0200

    Hexadezimal anzeigen

--- SevenSeg/SevenSeg.ino
+++ SevenSeg/SevenSeg.ino
@@ -1,56 +1,61 @@
 /*  TI_63_7Seg_ComK
-    7 Segment Display Test mit 5161AS (Einzelsegment) und
-    5641AS (Vierersegment).
+    7 Segment Display Test mit 5161AS (1x Display) und 5641AS (4x Displays).
     LEDs sind Positivaktiv
-    Vorwiderstand für LEDs jeweils 330 Ohm
+    Vorwiderstand für LEDs ist jeweils 330 Ohm
 
     Pin Belegung
-    LED   Einzelsegment Pin  Vierersegment Pin  ArduinoPin  MC_Pin
-    A     7                  11                 8           PB0
-    B     6                  7                  9           PB1
-    C     4                  4                  10          PB2
-    D     2                  2                  11          PB3
-    E     1                  1                  4           PD4
-    F     9                  10                 5           PD5
-    G     10                 5                  6           PD6
-    DP    5                  3                  7           PD7
-    GND0                     6                  A0          PC0
-    GND1                     8                  A1          PC1
-    GND2                     9                  A2          PC2
-    GND3                     12                 A3          PC3
-    GND4  8                                     A4          PC4
+    LED    5161AS    5641AS    ArduinoPin    MC_Pin
+    A      7         11        8             PB0
+    B      6         7         9             PB1
+    C      4         4         10            PB2
+    D      2         2         11            PB3
+    E      1         1         4             PD4
+    F      9         10        5             PD5
+    G      10        5         6             PD6
+    DP     5         3         7             PD7
+    GND0             6         A0            PC0
+    GND1             8         A1            PC1
+    GND2             9         A2            PC2
+    GND3             12        A3            PC3
+    GND4   8                   A4            PC4
 */
 
-#define TURN_OFF_SEGMENT 10
-// ungefähr 60Hz
-#define FRAME_TIME_MS 15
+#define TURN_OFF_DISPLAY 16
+// Zeit zwischen Wechsel der Anzeigen. 15ms entspricht ungefähr 60Hz
+#define DISPLAY_TIME_MS 3
 
-uint16_t current_number = 0;
-
-/* Zuweisungsliste: Zahl (0 - 9) zu LED Zustände; [10] => aus
+/* Zuweisungsliste: Hexadezimal (0 - 9, A - F) zu LED Zustände; [TURN_OFF_DISPLAY] => aus
+ Format:
  0b[PD7 - PD4][PB3 - PB0]
  0b[DP,G,F,E,D,C,B,A]
 */
-const uint8_t segment_assignments[] = {
-  0b00111111,
-  0b00000110,
-  0b01011011,
-  0b01001111,
-  0b01100110,
-  0b01101101,
-  0b01111101,
-  0b00000111,
-  0b01111111,
-  0b01101111,
+const uint8_t number_to_segments[] = {
+  0x3F, // 0
+  0x06, // 1
+  0x5B, // 2
+  0x4F, // 3
+  0x66, // 4
+  0x6D, // 5
+  0x7D, // 6
+  0x07, // 7
+  0x7F, // 8
+  0x6F, // 9
+  0x77, // A
+  0x7F, // B
+  0x39, // C
+  0x3F, // D
+  0x79, // E
+  0x71, // F
+  0x00  // Aus
 };
 
-/* Zeigt Zahl auf 7 Segment Display
-@param decimal_place Enables GND for each PCx pin
+/* Zeigt Zahl auf 7 Segment Display an. Unterstützt Hexadezimalzahlen.
+@param decimal_place Aktiviert GND für einen jeweiligen PCx Pin
 @returns 0 Erfolgreich; >1 Fehler
 */ 
 uint8_t display_one_number(uint8_t number, uint8_t decimal_place)
 {
-  if (number > 9)
+  if (number > 15)
   {
     return 1;
   }
@@ -60,11 +65,11 @@ uint8_t display_one_number(uint8_t number, uint8_t decimal_place)
     return 2;
   }
 
-  // GND aktivieren
+  // GND aktivieren; negativ Logik
   PORTC = ~(1 << decimal_place);
 
   // LED Zustand Code
-  uint8_t segment_assignment = segment_assignments[number];
+  uint8_t segment_assignment = number_to_segments[number];
 
   // LED pins
   // Using PB0 - PB3
@@ -75,33 +80,32 @@ uint8_t display_one_number(uint8_t number, uint8_t decimal_place)
   return 0;
 }
 
-void setup()
+// Von 0 bis 15 mit Hexadezimal Zählen. Auf Segment 0
+void test_one_segment()
 {
-  Serial.begin(9600);
-  Serial.println("7 Segment Display");
-
-  DDRB = 0x0F;
-  DDRD = 0xF0;
-  DDRC = 0b00011111;
-
-  // Ground Pins sind standartmäßig auf High
-  PORTC = 0b00011111;
+  for (uint16_t current_number = 0; current_number <= 15; current_number++)
+  {
+    display_one_number(current_number, 0);
+  }
 }
 
-void loop()
+// Zahl von 0 bis 99999 auf 5 Segmenten Anzeigen
+void display_number(uint16_t number)
 {
-  if (current_number > 9999)
+  if (number > 99999)
   {
-    current_number = 0;
+    return 1;
   }
 
-  uint8_t digit_1 = current_number % 10;
-  uint8_t digit_10 = (current_number / 10) % 10;
-  uint8_t digit_100 = (current_number / 100) % 10;
-  uint8_t digit_1000 = (current_number / 1000) % 10;
-  uint8_t digit_10000 = (current_number / 10000) % 10;
-
-  if (current_number < 1)
+  // Dezimalstellen auf verschiedene Anzeigen aufteilen
+  uint8_t digit_1 = number % 10;
+  uint8_t digit_10 = (number / 10) % 10;
+  uint8_t digit_100 = (number / 100) % 10;
+  uint8_t digit_1000 = (number / 1000) % 10;
+  uint8_t digit_10000 = (number / 10000) % 10;
+  
+  // Nullen vor der ersten aktiven Anzeige ausschalten
+  if (number < 1)
   {
     digit_10000 = TURN_OFF_SEGMENT;
     digit_1000 = TURN_OFF_SEGMENT;
@@ -109,42 +113,71 @@ void loop()
     digit_10 = TURN_OFF_SEGMENT;
     digit_1 = TURN_OFF_SEGMENT;
   }
-  else if (current_number < 10)
+  else if (number < 10)
   {
     digit_10000 = TURN_OFF_SEGMENT;
     digit_1000 = TURN_OFF_SEGMENT;
     digit_100 = TURN_OFF_SEGMENT;
     digit_10 = TURN_OFF_SEGMENT;
   }
-  else if (current_number < 100)
+  else if (number < 100)
   {
     digit_10000 = TURN_OFF_SEGMENT;
     digit_1000 = TURN_OFF_SEGMENT;
     digit_100 = TURN_OFF_SEGMENT;
   }
-  else if (current_number < 1000)
+  else if (number < 1000)
   {
     digit_10000 = TURN_OFF_SEGMENT;
     digit_1000 = TURN_OFF_SEGMENT;
   }
-  else if (current_number < 10000)
+  else if (number < 10000)
   {
     digit_10000 = TURN_OFF_SEGMENT;
   }
-
-  for (uint16_t waited_for_ms = 0; waited_for_ms < 200; waited_for_ms += FRAME_TIME_MS + 5)
+  
+  // Über das Schalten von GND werden die verschiedenen Segmente gesteuert.
+  // +5ms als geschätzter Wert für das Ausführen der Befehle.
+  // Könnte mit interrupt besser gestoppt werden.
+  for (uint16_t waited_for_ms = 0; waited_for_ms < 200; waited_for_ms += SEGMENT_TIME_MS + 5)
   {
     display_one_number(digit_1, 0);
-    delay(FRAME_TIME_MS / 5);
+    delay(SEGMENT_TIME_MS);
     display_one_number(digit_10, 1);
-    delay(FRAME_TIME_MS / 5);
+    delay(SEGMENT_TIME_MS);
     display_one_number(digit_100, 2);
-    delay(FRAME_TIME_MS / 5);
+    delay(SEGMENT_TIME_MS);
     display_one_number(digit_1000, 3);
-    delay(FRAME_TIME_MS / 5);
+    delay(SEGMENT_TIME_MS);
     display_one_number(digit_10000, 4);
-    delay(FRAME_TIME_MS / 5);
+    delay(SEGMENT_TIME_MS);
   }
+}
 
-  current_number++;
+// Von 0 bis 99999 Zählen
+void test_multiple_segments(uint16_t start_number)
+{
+  for (uint16_t current_number = start_number; current_number <= 99999; current_number++)
+  {
+    display_number(current_number);
+  }
+}
+
+void setup()
+{
+  Serial.begin(9600);
+  Serial.println("7 Segment Display");
+
+  DDRB = 0x0F;
+  DDRD = 0xF0;
+  DDRC = 0x1F;
+
+  // Ground Pins sind standardmäßig auf High
+  PORTC = 0x1F;
+}
+
+void loop()
+{
+  // test_multiple_segments(0);
+  test_one_segment();
 }
