e5ce8a4906b5498b9c8f34b936f68c5851d4c6a4
deanqx <110404616+deanqx@users.noreply.github.com>
2025-05-07 17:01:48 +0200

    Reorganised, less compatible

--- SevenSeg/SevenSeg.ino
+++ SevenSeg/SevenSeg.ino
@@ -28,6 +28,19 @@
 // number_to_segments[TURN_OFF_DISPLAY] == Aus 
 #define TURN_OFF_DISPLAY 16
 
+// Dezimalpunkt nach Stelle x
+#define DECIMAL_POINT_AFTER_PLACE -1
+// Zeit zwischen Wechsel der Anzeigen. 3ms * 5 = 15ms entspricht ungefähr 60Hz
+#define DISPLAY_TIME_MS 3
+// Verzögerung bis Zahl hochgezählt wird
+#define COUNTING_DELAY_MS 200
+
+// Hält Dezimalstellen und Vorzeichen
+typedef struct digits_t {
+  uint8_t d[5];
+  bool negative;
+};
+
 /* Zuweisungsliste: Hexadezimal (0 - 9, A - F) zu LED Zustände; [TURN_OFF_DISPLAY] => aus
  Format:
  0x[PD7 - PD4][PB4 - PB0]
@@ -58,11 +71,16 @@ byte port_b;
 byte port_c;
 byte port_d;
 
-// Hält Dezimalstellen und Vorzeichen
-typedef struct digits_t {
-  uint8_t d[5];
-  bool negative;
-};
+// Hält Zeitpunkt der letzten Ausführung (Für nicht blockierende Verzögerung)
+// INT32_MIN = 0b1000... was eine größe Zahl beim subtrahieren wird
+uint32_t prev_number_ms = INT32_MIN;
+uint32_t prev_display_ms = INT32_MIN;
+
+digits_t digits;
+
+// Werden hochgezählt bis zu den jeweiligen Limit
+int32_t curr_number = -10;
+uint8_t curr_digit = 0;
 
 /* Zeigt Zahl auf 7 Segment Display an. Unterstützt Hexadezimalzahlen.
  * @param decimal_place Aktiviert GND für einen jeweiligen PCx Pin
@@ -110,28 +128,73 @@ uint8_t display_single_number(uint8_t number, uint8_t decimal_place, bool decima
 
 // Maximum number is 99999
 // digits[n] => 10^n
-void int_to_digits(int32_t number, digits_t* digits)
+void int_to_digits(int32_t number)
 {
   // number ohne Vorzeichen
   const uint32_t number_abs = abs(number);
 
   // Dezimalstellen auf die verschiedenen Anzeigen aufteilen
-  digits->d[0] = number_abs % 10;
-  digits->d[1] = (number_abs / 10) % 10;
-  digits->d[2] = (number_abs / 100) % 10;
-  digits->d[3] = (number_abs / 1000) % 10;
-  digits->d[4] = (number_abs / 10000) % 10;
+  digits.d[0] = number_abs % 10;
+  digits.d[1] = (number_abs / 10) % 10;
+  digits.d[2] = (number_abs / 100) % 10;
+  digits.d[3] = (number_abs / 1000) % 10;
+  digits.d[4] = (number_abs / 10000) % 10;
   
   // Nullen vor der ersten aktiven Anzeige ausschalten
   // Die erste Anzeige bleibt immer aktiv
   int32_t d = 10000;
   for (uint8_t i = 4; i > 0 && d > number_abs; i--, d /= 10)
   {
-    digits->d[i] = TURN_OFF_DISPLAY;
+    digits.d[i] = TURN_OFF_DISPLAY;
   }
 
   // Negatives Vorzeichen
-  digits->negative = number < 0;
+  digits.negative = number < 0;
+}
+
+// Dezimalstellen anzeigen (non blocking delay)
+// @param decimal_point_after_place Deaktivieren mit -1
+void display_number(uint32_t curr_ms)
+{
+    if (curr_ms - prev_display_ms >= DISPLAY_TIME_MS)
+    {
+      prev_display_ms = curr_ms;
+
+      if (digits.negative)
+      {
+        port_c |= 1 << PC5;
+      }
+      else
+      {
+        port_c &= ~(1 << PC5);
+      }
+
+      display_single_number(digits.d[curr_digit], curr_digit, DECIMAL_POINT_AFTER_PLACE == curr_digit);
+
+      curr_digit++;
+      if (curr_digit > 4)
+      {
+        curr_digit = 0;
+      }
+    }
+}
+
+// Zahl hochzählen (non blocking delay)
+// @param decimal_point_after_place Deaktivieren mit -1
+void test_count_up(uint32_t curr_ms)
+{
+  if (curr_ms - prev_number_ms >= COUNTING_DELAY_MS)
+  {
+    prev_number_ms = curr_ms;
+
+    int_to_digits(curr_number);
+
+    curr_number++;
+    if (curr_number > 99999)
+    {
+      curr_number = -9999;
+    }
+  }
 }
 
 void setup()
@@ -143,77 +206,21 @@ void setup()
   DDRB = 0x0F;
   DDRC = 0x3F;
   DDRD = 0xF0;
+}
 
-  // Dezimalpunkt nach Stelle x
-  const int8_t decimal_point_after_place = -1;
-  // Zeit zwischen Wechsel der Anzeigen. 3ms * 5 = 15ms entspricht ungefähr 60Hz
-  const uint32_t display_time_ms = 3;
-  // Verzögerung bis Zahl hochgezählt wird
-  const uint32_t counting_delay_ms = 200;
-
-  // Hält Zeitpunkt der letzten Ausführung (Für nicht blockierende Verzögerung)
-  // INT32_MIN = 0b1000... was eine größe Zahl beim subtrahieren wird
-  uint32_t prev_number_ms = INT32_MIN;
-  uint32_t prev_display_ms = INT32_MIN;
-
-  digits_t digits;
-
-  // Werden hochgezählt bis zu den jeweiligen Limit
-  int32_t cur_number = -10;
-  uint8_t cur_digit = 0;
-
-  while (1)
-  {
+void loop()
+{
     port_b = PORTB;
     port_c = PORTC;
     port_d = PORTD;
 
     // current_ms - aktuelle Millisekunden
-    const uint32_t cur_ms = millis();
+    const uint32_t curr_ms = millis();
 
-    // Zahl hochählen (non blocking delay)
-    if (cur_ms - prev_number_ms >= counting_delay_ms)
-    {
-      prev_number_ms = cur_ms;
-
-      int_to_digits(cur_number, &digits);
-
-      cur_number++;
-      if (cur_number > 99999)
-      {
-        cur_number = -9999;
-      }
-    }
-
-    // Dezimalstellen anzeigen (non blocking delay)
-    if (cur_ms - prev_display_ms >= display_time_ms)
-    {
-      prev_display_ms = cur_ms;
-
-      if (digits.negative)
-      {
-        port_c |= 1 << PC5;
-      }
-      else
-      {
-        port_c &= ~(1 << PC5);
-      }
-
-      display_single_number(digits.d[cur_digit], cur_digit, decimal_point_after_place == (uint8_t) cur_digit);
-
-      cur_digit++;
-      if (cur_digit > 4)
-      {
-        cur_digit = 0;
-      }
-    }
+    test_count_up(curr_ms);
+    display_number(curr_ms);
 
     PORTB = port_b;
     PORTC = port_c;
     PORTD = port_d;
-  }
-}
-
-void loop()
-{
 }
