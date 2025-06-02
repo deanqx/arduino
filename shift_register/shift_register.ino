c4d62d487c7014ea17dcadd068941e293582d8ab
deanqx <110404616+deanqx@users.noreply.github.com>
2025-06-02 09:02:24 +0200

    Using register after shift register

--- shift_register/shift_register.ino
+++ shift_register/shift_register.ino
@@ -1,29 +1,41 @@
-/* ET_48_74HC164_Test
+/* ET_49_
  *
  * Pinbelegung
  *
- * Funktion | 74164  | Arduino | MC
- * Neg CLR  | 9        A1        PC0 
- * Data     | 1, 2     A2        PC1 
- * CLK      | 8        A3        PC2 
+ * Funktion | M74HC595B1 | Arduino | MC
+ * Neg CLR  | 10         | A1      | PC0 
+ * Data     | 14         | A2      | PC1 
+ * SRCLK    | 11         | A3      | PC2 
+ * RCLK     | 12         | A4      | PC4 
+ * Neg OE   | 13         | A5      | PC5 
+ *
+ * LA steht für Low Aktiv.
  */
 
-#define PIN_NEG_CLR 0
+#define PIN_CLR_LA 0
 #define PIN_DATA 1
 #define PIN_CLK 2
+#define PIN_RCLK 3
+#define PIN_OE_LA 4
+
 #define THIRD_CLK_MS 10
 
-// 21 entspricht 0100 1000
-const byte data = 0x48;
+// 21 entspricht 0100 1000 nach ET_48
+//const byte data = 0x48;
+
+// entspricht die Zahl 2
+// const byte data = 0b01011011;
+
+const byte data = 0x37; // 0011 0111
 
 void transmit_data() {
-  PORTC &= ~(1 << PIN_NEG_CLR);
+  PORTC &= ~(1 << PIN_CLR_LA);
   delay(THIRD_CLK_MS);
 
-  PORTC |= 1 << PIN_NEG_CLR;
+  PORTC |= 1 << PIN_CLR_LA;
   delay(THIRD_CLK_MS);
 
-  for (uint8_t bit_index = 0; bit_index < 8; bit_index++) {
+  for (int8_t bit_index = 7; bit_index >= 0; bit_index--) {
     // Serial data bit
     const byte data_bit = (data >> bit_index) & 0x01;
 
@@ -42,6 +54,12 @@ void transmit_data() {
     delay(THIRD_CLK_MS);
   }
 
+  PORTC |= 1 << PIN_RCLK;
+  delay(THIRD_CLK_MS);
+
+  PORTC &= ~(1 << PIN_RCLK);
+  delay(THIRD_CLK_MS);
+
   PORTB |= 1 << PB5;
 }
 
@@ -51,8 +69,9 @@ void setup() {
   // Use onboard LED
   DDRB = 1 << PB5;
 
-  DDRC = 0x07;
-  PORTC = 1 << PIN_NEG_CLR;
+  DDRC = 0x1F;
+  // OE = 0: alwas enable
+  PORTC = 1 << PIN_CLR_LA | 0 << PIN_OE_LA;
 
   transmit_data();
 }
