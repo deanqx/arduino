d6462c35b5da039d20171c6d3361b0eeb9abc01f
deanqx <110404616+deanqx@users.noreply.github.com>
2025-06-30 09:42:34 +0200

    Added 4 bit mode for LCD

---
+++ TI_79_LCD_4bit/src/main.c
@@ -0,0 +1,37 @@
+/*
+ * TI_74_AVRStudio_02.c
+ * LED Test PB5 + uart Ausgabe
+ *
+ * Created: 11.06.2025 08:37:34
+ * Author : JCSBK-User
+ */
+
+#define INFO "TI_74_AVRStudio_02\r\n"
+#define BAUD 19200UL // Baudrate Arduino
+
+#include "LCD_HD44780.h"
+#include "uartAT328p.h" // Einbinden der UART-Bibliothek,
+#include <avr/io.h>
+#include <util/delay.h>
+// diese muss sich in dem Verzeichnis der main.c befinden.
+
+int main(void) {
+  // Pinbelegung für lcd
+  DDRB = 0x3F;
+  DDRD = 0xF8;
+
+  usart_init(); // UART-Schnittstelle aktivieren PD0 = RX und PD1 = TX
+
+  lcd_init();
+  lcd_puts("test");
+
+  usart_puts(INFO);
+
+  while (1) {
+  }
+
+loop:
+  PORTB ^= (1 << PB5);
+  _delay_ms(100);
+  goto loop;
+}
