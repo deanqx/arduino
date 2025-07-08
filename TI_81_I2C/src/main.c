b427445ded9fe4390270fa239f3475f6bbe7e238
deanqx <110404616+deanqx@users.noreply.github.com>
2025-07-08 12:38:18 +0200

    I2C: Moved init and added comments

--- TI_81_I2C/src/main.c
+++ TI_81_I2C/src/main.c
@@ -22,20 +22,12 @@
 #define I2C_DDR_SCL DDRB
 #define I2C_DDR_SDA DDRB
 
-#define SET_BIT(port, pin) port |= 1 << pin;
-#define CLEAR_BIT(port, pin) port &= ~(1 << pin);
-
-void i2c_init_master(void) {
-  I2C_DDR_SCL |= 1 << I2C_SCL;
-  I2C_DDR_SDA |= 1 << I2C_SDA;
-  I2C_PORT_SCL |= 1 << I2C_SCL;
-  I2C_PORT_SDA |= 1 << I2C_SDA;
-}
-
 /*
- * Returns >0 when error ocurs
+ * Transmit 8 bits and check for acknowledge.
+ * @returns >0 when error ocurs
  * */
 uint8_t i2c_tx_byte(uint8_t data) {
+  // Transmit each bit with the MSB first
   for (int8_t i = 7; i >= 0; i--) {
     // Clear SDA bit and then set
     I2C_PORT_SDA = I2C_PORT_SDA & ~(1 << I2C_SDA) | ((data >> i) & 1)
@@ -62,17 +54,28 @@ uint8_t i2c_tx_byte(uint8_t data) {
 }
 
 /*
- * addr - 0100 001_ => 0x42
- * read - 0x01 => read; 0x00 => write
+ * Initialize as master and transmit start sequence
+ * @param addr - 0100 001_ => 0x42
+ * @param read - 0x01 => read; 0x00 => write
+ * @returns >0 when error ocurs
  * */
-void i2c_start(uint8_t addr, bool read) {
+uint8_t i2c_start(uint8_t addr, bool read) {
+  I2C_DDR_SCL |= 1 << I2C_SCL;
+  I2C_DDR_SDA |= 1 << I2C_SDA;
+
+  I2C_PORT_SCL |= 1 << I2C_SCL;
   I2C_PORT_SDA &= ~(1 << I2C_SDA);
   _delay_us(I2C_T_QUARTER_US);
+
   I2C_PORT_SCL &= ~(1 << I2C_SCL);
   _delay_us(I2C_T_QUARTER_US);
 
-  i2c_tx_byte(addr | read);
+  return i2c_tx_byte(addr | read);
 }
+
+/*
+ * Send stop sequence
+ * */
 void i2c_stop(void) {
   I2C_PORT_SCL |= 1 << I2C_SCL;
   _delay_us(I2C_T_QUARTER_US);
@@ -81,13 +84,12 @@ void i2c_stop(void) {
 }
 
 int main(void) {
-  i2c_init_master();
-
   // Slave address (0x42) + write
   i2c_start(0x42, false);
 
   // Send byte
   i2c_tx_byte(0xAA);
 
+  // Send stop sequence
   i2c_stop();
 }
