98d43063616457ab62531f31dacecb3ed660ac1c
deanqx <110404616+deanqx@users.noreply.github.com>
2025-08-28 13:43:46 +0200

    WIP: TI_82

---
+++ TI_82_I2C_diskret_unsereBib/src/main.c
@@ -0,0 +1,32 @@
+#define F_CPU 16000000UL
+#define BAUD 19200UL
+
+#include "i2c.h"
+#include "uartAT328p.h"
+#include <stdint.h>
+#include <util/delay.h>
+
+#define READER_ADDR 0x71
+
+uint8_t read_count_start(void) {
+  i2c_start();
+  i2c_send(0x74);
+  uint8_t count_start = i2c_receive(0);
+  i2c_stop();
+
+  return count_start;
+}
+
+int main(void) {
+  usart_init();
+  i2c_init();
+
+  uint8_t count_start = read_count_start();
+  usart_itoa(count_start);
+  usart_puts("\r\n");
+
+  i2c_write(PCF8574_ADR, 0x00, 0x00, ~count_start, PCF8574_TYP);
+
+  return 0;
+  i2c_sendeByte(0x40, 0xAA);
+}
