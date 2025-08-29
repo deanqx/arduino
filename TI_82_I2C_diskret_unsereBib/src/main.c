26ba7e0559226e481686eb16288c670cda68971b
deanqx <110404616+deanqx@users.noreply.github.com>
2025-08-29 10:47:49 +0200

    Searching why reading not working

--- TI_82_I2C_diskret_unsereBib/src/main.c
+++ TI_82_I2C_diskret_unsereBib/src/main.c
@@ -20,13 +20,24 @@ uint8_t read_count_start(void) {
 int main(void) {
   usart_init();
   i2c_init();
+  i2c_start();
+
+  while (1) {
+    i2cPORT |= (1 << i2cSDApin);  // SDA auf High
+    _delay_us(120000);            // Pause
+    i2cPORT &= ~(1 << i2cSDApin); // SDA auf Low
+    _delay_us(120000);            // Pause
+    usart_puts("test\r\n");
+  }
+
+  i2c_write(PCF8574_ADR, 0x00, 0x00, 0xAA, PCF8574_TYP);
+  return 0;
+  i2c_start();
+  return 0;
 
   uint8_t count_start = read_count_start();
   usart_itoa(count_start);
   usart_puts("\r\n");
 
   i2c_write(PCF8574_ADR, 0x00, 0x00, ~count_start, PCF8574_TYP);
-
-  return 0;
-  i2c_sendeByte(0x40, 0xAA);
 }
