2107ac072213b9f6f4d684fd338e85ca108baf5e
deanqx <110404616+deanqx@users.noreply.github.com>
2025-09-05 10:41:53 +0200

    fix(i2c library): send start condition, send ACK, set DDR and more

--- TI_82_I2C_diskret_unsereBib/src/main.c
+++ TI_82_I2C_diskret_unsereBib/src/main.c
@@ -3,41 +3,26 @@
 
 #include "i2c.h"
 #include "uartAT328p.h"
+#include <stdbool.h>
 #include <stdint.h>
 #include <util/delay.h>
 
-#define READER_ADDR 0x71
-
-uint8_t read_count_start(void) {
-  i2c_start();
-  i2c_send(0x74);
-  uint8_t count_start = i2c_receive(0);
-  i2c_stop();
-
-  return count_start;
-}
+#define READER_ADDR 0x04
+#define DISPLAY_ADDR 0x00
 
 int main(void) {
   usart_init();
+
+  DDRB = 1 << i2cSCLpin | 1 << i2cSDApin;
   i2c_init();
-  i2c_start();
 
-  while (1) {
-    i2cPORT |= (1 << i2cSDApin);  // SDA auf High
-    _delay_us(120000);            // Pause
-    i2cPORT &= ~(1 << i2cSDApin); // SDA auf Low
-    _delay_us(120000);            // Pause
-    usart_puts("test\r\n");
-  }
+  // inverting pins because it is easier to pull
+  uint8_t input = ~i2c_read(PCF8574A_ADR, READER_ADDR, 0, PCF8574A_TYP);
 
-  i2c_write(PCF8574_ADR, 0x00, 0x00, 0xAA, PCF8574_TYP);
-  return 0;
-  i2c_start();
+  // inverting displayed value because leds are in negative logic
+  i2c_write(PCF8574_ADR, DISPLAY_ADDR, 0, ~input, PCF8574_TYP);
+
   return 0;
 
-  uint8_t count_start = read_count_start();
-  usart_itoa(count_start);
-  usart_puts("\r\n");
-
-  i2c_write(PCF8574_ADR, 0x00, 0x00, ~count_start, PCF8574_TYP);
+  // i2c_write(PCF8574_ADR, 0x00, 0x00, ~count_start, PCF8574_TYP);
 }
