d1bfd0c406c7b431269195eadb7b61fa868c7349
deanqx <110404616+deanqx@users.noreply.github.com>
2026-03-13 08:54:16 +0100

    feat(ET_58): add arduino test

---
+++ ET_58_Klausur/src/main.c
@@ -0,0 +1,94 @@
+/*
+ * ZWL:
+ * Arduino  AVR     Beschreibung
+ * A0       PC0     /S0 LA
+ * A1       PC1     S1 LA
+ * A2       PC2     S2 LA
+ * A3       PC3     S3 LA
+ * D8       PC4     E2 LA
+ * D9       PC5     E1 LA
+ * D2       PD2     Q3
+ * D3       PD3     Q2
+ * D4       PD4     Q1
+ *
+ * LA = Low Active
+ */
+
+#include "usart.h"
+#include <avr/io.h>
+#include <stdbool.h>
+
+static const uint8_t NOTAUS = PC0;
+static const uint8_t TISCH = PC1;
+static const uint8_t AUS = PC2;
+static const uint8_t STEIN = PC3;
+static const uint8_t E2 = PC4;
+static const uint8_t E1 = PC5;
+static const uint8_t STEIN_MOTOR = PD2;
+static const uint8_t TISCH_ZU_E2 = PD3;
+static const uint8_t TISCH_ZU_E1 = PD4;
+
+#define READ_PIN(x) (PINC >> x & 1)
+
+int main(void) {
+  // Pull-Up aktivieren
+  PORTC = 1 << NOTAUS | 1 << TISCH | 1 << AUS | 1 << STEIN | 1 << E1 | 1 << E2;
+  // Ausgänge setzen
+  DDRD = 1 << STEIN_MOTOR | 1 << TISCH_ZU_E2 | 1 << TISCH_ZU_E1;
+
+  uart0_init(BAUD_CALC(9600UL));
+
+  // zwischen Speicher der Zustände
+  bool notaus, tisch, aus, stein, e1, e2;
+  bool stein_motor = false, tisch_zu_e2 = false, tisch_zu_e1 = false;
+
+  while (1) {
+    // --- Einlesen ---
+    notaus = !READ_PIN(NOTAUS);
+    tisch = READ_PIN(TISCH);
+    aus = READ_PIN(AUS);
+    stein = READ_PIN(STEIN);
+    e1 = READ_PIN(E1);
+    e2 = READ_PIN(E2);
+
+    // --- Verarbeitung ---
+    if (notaus || aus) {
+      uart_puts("Notaus oder aus");
+      stein_motor = false;
+      tisch_zu_e2 = false;
+      tisch_zu_e1 = false;
+    } else {
+      if (tisch && !tisch_zu_e1) {
+        uart_puts("Tisch wird zum ersten mal gestartet");
+        tisch_zu_e2 = true;
+      }
+
+      if (stein) { // Stein aktivieren?
+        uart_puts("Stein anschalten");
+        stein_motor = true;
+      }
+
+      // Stein abschalten wenn tisch abgeschaltet
+      if (!(tisch_zu_e1 || tisch_zu_e2)) {
+        uart_puts("Stein abschalten");
+        stein_motor = false;
+      }
+
+      if (e1 && tisch_zu_e1) {
+        uart_puts("Tisch richtung wechseln nach rechts\n");
+        tisch_zu_e1 = false;
+        tisch_zu_e2 = true;
+      }
+
+      if (e2 && tisch_zu_e2) {
+        uart_puts("Tisch richtung wechseln nach links\n");
+        tisch_zu_e1 = true;
+        tisch_zu_e2 = false;
+      }
+    }
+
+    // --- Ausgabe ---
+    PORTD = stein_motor << STEIN_MOTOR | tisch_zu_e2 << TISCH_ZU_E2 |
+            tisch_zu_e1 << TISCH_ZU_E1;
+  }
+}
