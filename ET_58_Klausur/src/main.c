fb1f412230e12753ce80cb56c27790fe26888c2e
deanqx <110404616+deanqx@users.noreply.github.com>
2026-03-13 09:24:56 +0100

    feat(ET_58): add fuses

--- ET_58_Klausur/src/main.c
+++ ET_58_Klausur/src/main.c
@@ -1,12 +1,14 @@
 /*
  * ZWL:
  * Arduino  AVR     Beschreibung
+ * D8       PB0     /F7 LA
+ * D9       PB1     /F11 LA
  * A0       PC0     /S0 LA
  * A1       PC1     S1 LA
  * A2       PC2     S2 LA
  * A3       PC3     S3 LA
- * D8       PC4     E2 LA
- * D9       PC5     E1 LA
+ * A4       PC4     E2 LA
+ * A5       PC5     E1 LA
  * D2       PD2     Q3
  * D3       PD3     Q2
  * D4       PD4     Q1
@@ -18,12 +20,16 @@
 #include <avr/io.h>
 #include <stdbool.h>
 
+static const uint8_t FUSE_TISCH = PB0;
+static const uint8_t FUSE_STEIN = PB1;
+
 static const uint8_t NOTAUS = PC0;
 static const uint8_t TISCH = PC1;
 static const uint8_t AUS = PC2;
 static const uint8_t STEIN = PC3;
 static const uint8_t E2 = PC4;
 static const uint8_t E1 = PC5;
+
 static const uint8_t STEIN_MOTOR = PD2;
 static const uint8_t TISCH_ZU_E2 = PD3;
 static const uint8_t TISCH_ZU_E1 = PD4;
@@ -31,6 +37,8 @@ static const uint8_t TISCH_ZU_E1 = PD4;
 #define READ_PIN(x) (PINC >> x & 1)
 
 int main(void) {
+  // Pull-Up aktivieren
+  PORTB = 1 << FUSE_TISCH | 1 << FUSE_STEIN;
   // Pull-Up aktivieren
   PORTC = 1 << NOTAUS | 1 << TISCH | 1 << AUS | 1 << STEIN | 1 << E1 | 1 << E2;
   // Ausgänge setzen
@@ -58,7 +66,10 @@ int main(void) {
       tisch_zu_e2 = false;
       tisch_zu_e1 = false;
     } else {
-      if (tisch && !tisch_zu_e1) {
+      if (~PINB >> FUSE_TISCH & 1) {
+        tisch_zu_e2 = false;
+        tisch_zu_e1 = false;
+      } else if (tisch && !tisch_zu_e1) {
         uart_puts("Tisch wird zum ersten mal gestartet");
         tisch_zu_e2 = true;
       }
@@ -69,7 +80,7 @@ int main(void) {
       }
 
       // Stein abschalten wenn tisch abgeschaltet
-      if (!(tisch_zu_e1 || tisch_zu_e2)) {
+      if (!(tisch_zu_e1 || tisch_zu_e2) || ~PINB >> FUSE_STEIN & 1) {
         uart_puts("Stein abschalten");
         stein_motor = false;
       }
